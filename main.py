32764b239782ff2f4a22f97a96528deea604d0d8
aleortega <aleortega@cofares.es>
2020-04-19 17:52:14 +0200

    Se actualiza el news_translator para tirar de google

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ Created on Sun Apr 19 10:16:28 2020
 from scraping import News
 from news_translator import NewsTranslated
 import pandas as pd
-
-#%%              EXTRACTION OF ARTICLES
-
+             
+# %%==========================================================================
+#                               EXTRACTION OF ARTICLES
+# =============================================================================
 urls_generales = ['https://www.elmundo.es/']
 
 urls_especificas = ['https://towardsdatascience.com','https://machinelearningmastery.com','http://news.mit.edu/topic/machine-learning']
@@ -22,25 +23,9 @@ Extraccion.save_as_csv("ML_1")
 Extraccion.save_as_xlsx("ML_1")
 
 
-# %%===========================================================================
-#     TRANSLATE ARTICLES
+# %% ==========================================================================
+#                              TRANSLATE ARTICLES
 # =============================================================================
 noticias_ml = NewsTranslated(news_file = 'DATA/ML_1.csv')
-a = noticias_ml.translate()
-
-# %%
-articulos = []
-articulos = pd.read_csv(r"Data/ML_1.csv",sep = ';')
-#articulos = articulos[2:3]
-articulos = articulos.dropna(subset=['titulo', 'cuerpo'])
-
-articulos['cuerpo'] = articulos['cuerpo'].apply(auto_truncate)
-
-
-translator = Translator()
-articulos['titulo_traducido'] = articulos['titulo'].apply(translator.translate, src='en', dest='es').apply(getattr, args=('text',))
-
-#articulos['cuerpo_traducido'] = articulos['cuerpo'].apply(translator.translate, src='en', dest='es').apply(getattr, args=('text',))
-
-articulos.to_excel(r'DATA/ML1_translated'+".xlsx", encoding="utf-8")
-articulos.to_csv(r'DATA/ML1_translated'+".csv", encoding="utf-8", sep = ';')
+df_traducidos = noticias_ml.translate_articles_df()
+noticias_ml.save_df()
--- news_translator.py
+++ news_translator.py
@@ -8,31 +8,75 @@ Created on Sun Apr 19 12:10:23 2020
 
 import pandas as pd
 from googletrans import Translator
+from google.cloud import translate_v2 as translate
+import time
+
 
 # %%
 
 def auto_truncate(val):
-    return val[:4999]
+    return val[:9999]
 
 class NewsTranslated():
     def __init__(self, news_file):
         self.articulos = pd.read_csv(r"Data/ML_1.csv",sep = ';')
-        #self.articulos = self.articulos[10:11]
+        self.articulos = self.articulos[:50]
+        self.articulos = self.articulos.drop_duplicates(subset='titulo', keep="first")
         self.articulos = self.articulos.dropna(subset=['titulo', 'cuerpo'])
         self.articulos['cuerpo'] = self.articulos['cuerpo'].apply(auto_truncate)
+        self.translate_client = translate.Client()
+        
+       
+    def translate_articles_df(self):
+        self.df_traducidos = pd.DataFrame(columns=["periodico","url","fecha","titulo","cuerpo","titulo_traducido","cuerpo_traducido"])
+        
+        for index, row in self.articulos.iterrows():
+            try:
+                tr_titulo= row['titulo']
+                tr_cuerpo= row['cuerpo']
+                time.sleep(0.5)
+                target = 'es'
+                translation = self.translate_client.translate(tr_titulo,target_language=target)
+                tr_titulo = format(translation['translatedText'])
+                
+                translation = self.translate_client.translate(tr_cuerpo,target_language=target)
+                tr_cuerpo = format(translation['translatedText'])
     
-    def translate(self):
-        translator = Translator()
-        self.articulos['titulo_traducido'] = self.articulos['titulo'].apply(translator.translate, src='en', dest='es').apply(getattr, args=('text',))
-        
-        self.articulos['cuerpo_traducido'] = self.articulos['cuerpo'].apply(translator.translate, src='en', dest='es').apply(getattr, args=('text',))
-        return self.articulos
-        
-        
+                self.df_traducidos = self.df_traducidos.append({"periodico": row['periodico'], 
+                                                      "url":row['url'], 
+                                                      "fecha":row['fecha'], 
+                                                      "titulo": row['titulo'],
+                                                      "cuerpo":row['cuerpo'],
+                                                      "titulo_traducido" : tr_titulo, 
+                                                      "cuerpo_traducido": tr_cuerpo}, ignore_index = True)
+                
+                if index % 10 == 1:
+                    print("Noticia Nº: " + str(index) + " Traducida")
+
+            except Exception as err:
+                time.sleep(10)
+                print("Error al traducir noticia Nº: " + str(index))
+                
+        return self.df_traducidos
         
     def save_df(self):
-        self.articulos.to_excel(r'DATA/ML1_translated'+".xlsx", encoding="utf-8")
-        
-        
+        self.df_traducidos.to_excel(r'DATA/ML1_translated'+".xlsx", encoding="utf-8")
+        self.df_traducidos.to_csv(r'DATA/ML1_translated'+".csv", encoding="utf-8", sep = ';')
 
-   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
